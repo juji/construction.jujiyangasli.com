4b01119c59b84eb5b1b9ad7dd109cbd40f23d60c
juji <jujiyangasli@gmail.com>
2022-02-21 19:35:12 +0700

    ssr

--- components/Ball/index.js
+++ components/Ball/index.js
@@ -1,4 +1,4 @@
 import dynamic from "next/dynamic";
 
-const BallCanvas = dynamic(import("components/Ball/BallCanvas"));
+const BallCanvas = dynamic(import("components/Ball/BallCanvas"),{ ssr: false });
 export default BallCanvas
