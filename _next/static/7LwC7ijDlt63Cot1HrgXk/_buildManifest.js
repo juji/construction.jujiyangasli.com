6f433c75d659cf0c151acc5d7b733f936647258a
juji <juji@users.noreply.github.com>
2022-02-21 05:22:17 +0000

    deploy: 6749f938512b273702c8ea4d28b09d6671b22a32

---
+++ _next/static/7LwC7ijDlt63Cot1HrgXk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b1048d3d5e7a3e79.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
